e0434ca0df83d6325bb4479e3e506ebfe5655e2a
Tohidul29 <tohidswfu@gmail.com>
2022-03-06 16:42:02 +0600

    bg color change using js

---
+++ app.js
@@ -0,0 +1,42 @@
+let headerText = document.getElementById('header-text');
+
+const blueColor = () => {
+    document.body.style.backgroundColor = 'blue';
+    headerText.style.color = 'white';
+};
+
+const grayColor = () => {
+    document.body.style.backgroundColor = 'gray';
+    headerText.style.color = 'white';
+};
+
+const greenColor = () => {
+    document.body.style.backgroundColor = 'green';
+    headerText.style.color = 'white';
+};
+
+const redColor = () => {
+    document.body.style.backgroundColor = 'red';
+    headerText.style.color = 'white';
+};
+
+const yellowColor = () => {
+    document.body.style.backgroundColor = 'yellow';
+    headerText.style.color = 'black';
+};
+
+const skyBlueColor = () => {
+    document.body.style.backgroundColor = 'skyblue';
+    headerText.style.color = 'black';
+};
+
+const whiteColor = () => {
+    document.body.style.backgroundColor = 'white';
+    headerText.style.color = 'black';
+};
+
+const blackColor = () => {
+    document.body.style.backgroundColor = 'black';
+    headerText.style.color = 'white';
+};
+
